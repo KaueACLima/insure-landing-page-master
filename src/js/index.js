7b7a0fa6cfe8ab51fed26ebc16bb92ff67f7464c
Kauê Lima <kaue5939.lima@gmail.com>
2024-10-21 19:38:10 -0300

    Update CSS/index.css

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 const hamburguer = document.querySelector(".hamburguer");
 const desktopMenu = document.querySelector(".desktop-menu");
 
-hamburguer.addEventListener('click', function() {
+hamburguer.addEventListener("click", function () {
   desktopMenu.classList.toggle("hidden");
-  hamburguer.src = "../images/icon-close.svg";
+  hamburguer.src = desktopMenu.classList.contains("hidden")
+    ? "./src/images/icon-hamburger.svg"
+    : "../images/icon-close.svg";
 });
